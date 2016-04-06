abd87279571a6ee3f9eda6353fd5fbbe7d235986
klenzo <crea2luxe@gmail.com>
2016-04-06 22:39:57 +0200

    Transfert dans src

--- app/views/inc/head.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title><?= $_PAGE->getInfo('nameProject'); ?></title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="/assets/css/style.css">
-</head>
-<body>
--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once('../app/config/config.php');
-
-	require_once('../app/controllers/home.controllers.php');
-	require_once('../app/core/home.core.php');
-
-	require_once('../app/views/inc/head.php');
-	require_once('../app/views/home.php');
-	require_once('../app/views/inc/footer.php');
